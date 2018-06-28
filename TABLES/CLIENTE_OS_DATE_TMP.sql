a4905e8072840f5d7405a56063e51742f1d85815
Costafortes <36734970+costafortes@users.noreply.github.com>
2018-06-28 13:29:56 -0400

    Migração inicial2
    added by jlacf

---
+++ TABLES/CLIENTE_OS_DATE_TMP.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------
+--  DDL for Table CLIENTE_OS_DATE_TMP
+--------------------------------------------------------
+
+  CREATE GLOBAL TEMPORARY TABLE "WS4TI"."CLIENTE_OS_DATE_TMP" 
+   (	"TMP_DATE" DATE
+   ) ON COMMIT DELETE ROWS ;
